d5cbf8864a7c51c15cf6bfbe15a37828c34767e9
聡志 渥美 <pharaoh.jpn@gmail.com>
2019-06-20 02:07:39 +0900

    Version 2.1

--- options/options.js
+++ options/options.js
@@ -12,6 +12,9 @@
 'use strict';
 
 var xmlHttpRequest = new XMLHttpRequest();
+xmlHttpRequest.open('GET', browser.extension.getURL('/_values/PageActions.json'), false);
+xmlHttpRequest.send();
+const pageActions = JSON.parse(xmlHttpRequest.responseText);
 xmlHttpRequest.open('GET', browser.extension.getURL('/_values/SearchEngines.json'), false);
 xmlHttpRequest.send();
 const searchEngines = JSON.parse(xmlHttpRequest.responseText);
@@ -19,47 +22,57 @@ xmlHttpRequest.open('GET', browser.extension.getURL('/_values/StorageKeys.json')
 xmlHttpRequest.send();
 const storageKeys = JSON.parse(xmlHttpRequest.responseText);
 
-const askRadioId = 'searcheEngineAsk';
-const bingRadioId = 'searcheEngineBing';
-const duckduckgoRadioId = 'searcheEngineDuckDuckGo';
-const googleRadioId = 'searcheEngineGoogle';
-const yahooRadioId = 'searcheEngineYahoo';
-const yahooJapanRadioId = 'searcheEngineYahooJapan';
+const searcheEngineAskId = 'searcheEngineAsk';
+const searcheEngineBingId = 'searcheEngineBing';
+const searcheEngineDuckDuckGoId = 'searcheEngineDuckDuckGo';
+const searcheEngineGoogleId = 'searcheEngineGoogle';
+const searcheEngineYahooId = 'searcheEngineYahoo';
+const searcheEngineYahooJapanId = 'searcheEngineYahooJapan';
+const pageActionReloadId = 'pageActionReload';
+const pageActionGoBackToHomeId = 'pageActionGoBackToHome';
 
 document.getElementsByTagName('html')[0].lang = browser.i18n.getUILanguage();
 document.title = browser.i18n.getMessage('optionsHTMLTitle');
-document.getElementById('SearchEngines').innerText = browser.i18n.getMessage('searchEngines');
+document.getElementById('searchEnginesLegend').innerText = browser.i18n.getMessage('searchEngines');
 document.getElementById('askSearchEngineLabel').innerText = browser.i18n.getMessage('ask');
 document.getElementById('alwaysUseBingLabel').innerText = browser.i18n.getMessage('alwaysUseBing');
 document.getElementById('alwaysUseDuckDuckGoLabel').innerText = browser.i18n.getMessage('alwaysUseDuckDuckGo');
 document.getElementById('alwaysUseGoogleLabel').innerText = browser.i18n.getMessage('alwaysUseGoogle');
 document.getElementById('alwaysUseYahooLabel').innerText = browser.i18n.getMessage('alwaysUseYahoo');
 document.getElementById('alwaysUseYahooJapanLabel').innerText = browser.i18n.getMessage('alwaysUseYahooJapan');
+document.getElementById('pageActionLegend').innerText = browser.i18n.getMessage('behaviorOfPageAction');
+document.getElementById('pageActionReloadLabel').innerText = browser.i18n.getMessage('reload');
+document.getElementById('pageActionGoBackToHomeLabel').innerText = browser.i18n.getMessage('goBackToHome');
 
 document.options.searchEngine.forEach((element) => {
-	element.addEventListener('click', searchEngineOnClick);
+	element.addEventListener('click', searchEngineRadioButtonOnClick);
+});
+
+document.options.pageAction.forEach((element) => {
+	element.addEventListener('click', pageActionRadioButtonOnClick);
 });
 
 checkSearchEngine();
+checkPageAction();
 
-function searchEngineOnClick(event) {
+function searchEngineRadioButtonOnClick(event) {
 	switch (event.target.id) {
-		case askRadioId:
+		case searcheEngineAskId:
 			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.ask.name });
 			break;
-		case bingRadioId:
+		case searcheEngineBingId:
 			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.bing.name });
 			break;
-		case duckduckgoRadioId:
+		case searcheEngineDuckDuckGoId:
 			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.duckduckgo.name });
 			break;
-		case googleRadioId:
-			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.google.name});
+		case searcheEngineGoogleId:
+			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.google.name });
 			break;
-		case yahooRadioId:
-			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.yahoo.name});
+		case searcheEngineYahooId:
+			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.yahoo.name });
 			break;
-		case yahooJapanRadioId:
+		case searcheEngineYahooJapanId:
 			browser.storage.local.set({ [storageKeys.searchEngine]: searchEngines.yahooJapan.name });
 			break;
 	}
@@ -72,21 +85,47 @@ function checkSearchEngine() {
 		if (Object.keys(item).includes(storageKeys.searchEngine)) {
 			switch (item[storageKeys.searchEngine]) {
 				case searchEngines.bing.name:
-					document.getElementById(bingRadioId).checked = true;
+					document.getElementById(searcheEngineBingId).checked = true;
 					break;
 				case searchEngines.duckduckgo.name:
-					document.getElementById(duckduckgoRadioId).checked = true;
+					document.getElementById(searcheEngineDuckDuckGoId).checked = true;
 					break;
 				case searchEngines.google.name:
-					document.getElementById(googleRadioId).checked = true;
+					document.getElementById(searcheEngineGoogleId).checked = true;
 					break;
 				case searchEngines.yahoo.name:
-					document.getElementById(yahooRadioId).checked = true;
+					document.getElementById(searcheEngineYahooId).checked = true;
 					break;
 				case searchEngines.yahooJapan.name:
-					document.getElementById(yahooJapanRadioId).checked = true;
+					document.getElementById(searcheEngineYahooJapanId).checked = true;
 					break;
 			}
+		}
+	});
+}
+
+function pageActionRadioButtonOnClick(event) {
+	switch (event.target.id) {
+		case pageActionReloadId:
+			browser.storage.local.set({ [storageKeys.pageAction]: pageActions.reload });
+			break;
+		case pageActionGoBackToHomeId:
+			browser.storage.local.set({ [storageKeys.pageAction]: pageActions.goBackToHome });
+			break;
+	}
+
+	checkPageAction();
+}
+
+function checkPageAction() {
+	browser.storage.local.get(storageKeys.pageAction).then((item) => {
+		switch (item[storageKeys.pageAction]) {
+			case pageActions.reload:
+				document.getElementById(pageActionReloadId).checked = true;
+				break;
+			case pageActions.goBackToHome:
+				document.getElementById(pageActionGoBackToHomeId).checked = true;
+				break;
 		}
 	});
 }
--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -11,12 +11,46 @@
  */
 'use strict';
 
+var xmlHttpRequest = new XMLHttpRequest();
+xmlHttpRequest.open('GET', browser.extension.getURL('/_values/PageActions.json'), false);
+xmlHttpRequest.send();
+const pageActions = JSON.parse(xmlHttpRequest.responseText);
+xmlHttpRequest.open('GET', browser.extension.getURL('/_values/SearchEngines.json'), false);
+xmlHttpRequest.send();
+const searchEngines = JSON.parse(xmlHttpRequest.responseText);
+xmlHttpRequest.open('GET', browser.extension.getURL('/_values/StorageKeys.json'), false);
+xmlHttpRequest.send();
+const storageKeys = JSON.parse(xmlHttpRequest.responseText);
+
 document.getElementsByTagName('html')[0].lang = browser.i18n.getUILanguage();
 document.title = browser.i18n.getMessage('sidebarHTMLTitle');
 
-browser.sidebarAction.getPanel({}).then((sidebarUrl) => {
-	if (sidebarUrl === browser.extension.getURL(`sidebar/sidebar.html`)) {
+browser.storage.local.get([storageKeys.pageAction, storageKeys.searchEngine]).then((item) => {
+	if (item === undefined) {
+		window.location = browser.extension.getURL("index.html");
+	} else if (item[storageKeys.pageAction] === pageActions.goBackToHome) {
+		switch (item[storageKeys.searchEngine]) {
+			case searchEngines.bing.name:
+				window.location = searchEngines.bing.url;
+				break;
+			case searchEngines.duckduckgo.name:
+				window.location = searchEngines.duckduckgo.url;
+				break;
+			case searchEngines.google.name:
+				window.location = searchEngines.google.url;
+				break;
+			case searchEngines.yahoo.name:
+				window.location = searchEngines.yahoo.url;
+				break;
+			case searchEngines.yahooJapan.name:
+				window.location = searchEngines.yahooJapan.url;
+				break;
+			default:
+				window.location = browser.extension.getURL("index.html");
+		}
+	} else {
 		window.location = browser.extension.getURL("index.html");
 	}
+}).catch(() => {
+	window.location = browser.extension.getURL("index.html");
 });
-
